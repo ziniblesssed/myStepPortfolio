50f75a56762f2d25c1c0b336fb136379b50ba5ec
Adedeji <adedejiadebisi01@gmail.com>
2020-06-08 15:46:44 +0000

    Added google map Api to contact page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -83,3 +83,8 @@ async function deleteComment() {
   await fetch('/delete-data', {method: 'POST'});
   location.reload();
 }
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
