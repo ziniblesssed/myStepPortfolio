e9e4729be2ffba364840a8a847a79313557a6649
Adedeji <adedejiadebisi01@gmail.com>
2020-06-10 15:09:16 +0000

    Implemented a map page that shows searches in lagos and displays my football pitch marker

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -264,3 +264,64 @@ AutocompleteDirectionsHandler.prototype.route = function() {
             }
         });
 };
+
+
+function initMap2() {
+    // Create the map.
+    var lagos = {lat: 6.458985, lng: 3.601521};
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: lagos,
+        zoom: 1
+    });
+
+    // Create the places service.
+    var service = new google.maps.places.PlacesService(map);
+    var getNextPage = null;
+    var moreButton = document.getElementById('more');
+    moreButton.onclick = function() {
+        moreButton.disabled = true;
+        if (getNextPage) getNextPage();
+    };
+
+    // Perform a nearby search.
+    service.nearbySearch(
+        {location: lagos, radius: 2000, type: ['store']},
+        function(results, status, pagination) {
+            if (status !== 'OK') return;
+
+            createMarkers(results);
+            moreButton.disabled = !pagination.hasNextPage;
+            getNextPage = pagination.hasNextPage && function() {
+            pagination.nextPage();
+            };
+        });
+}
+
+function createMarkers(places) {
+    var bounds = new google.maps.LatLngBounds();
+    var placesList = document.getElementById('places');
+
+    for (var i = 0, place; place = places[i]; i++) {
+        var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+    };
+
+    var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+    });
+
+        var li = document.createElement('li');
+        li.textContent = place.name;
+        placesList.appendChild(li);
+
+        bounds.extend(place.geometry.location);
+    }
+    map.fitBounds(bounds);
+}
