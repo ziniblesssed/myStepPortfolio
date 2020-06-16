62ef1067bd7add80725cb1d87de734adbb968630
Adedeji <adedejiadebisi01@gmail.com>
2020-06-16 01:17:37 +0000

    Fixed PR comment

--- walkthroughs/week-5-tdd/intro/src/main/java/com/google/sps/Greeter.java
+++ walkthroughs/week-5-tdd/intro/src/main/java/com/google/sps/Greeter.java
@@ -22,7 +22,7 @@ public class Greeter {
    * Returns a greeting for the given name.
    */
   public String greet(String name) {
-      name = name.replaceAll(" ","");
+      name = name.replaceAll("\\s","");
     return "Hello " + name;
   }
 }
